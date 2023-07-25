0513358a19e6b60220a4a24e8430f0e39945c80c
Sjlowman <sarah.j.lowman@gmail.com>
2023-07-25 19:25:05 +0100

    Implement passowrd length classification

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const checkPwd = () => {
   const pwd = pwdInput.value;
   const pwdLength = document.getElementById("pwd").value.length;
+  console.log(pwdLength);
   let pwdValidity = "invalid";
-  if (pwdLength > 7 && pwdValidity < 12) {
+  if (pwdLength < 8) {
+    pwdValidity = "invalid";
+  } else if (pwdLength < 12) {
     pwdValidity = "valid";
   } else if (pwdLength > 11) {
     pwdValidity = "optimal";
   }
-
+  console.log(pwdValidity);
   /* Add check here.  Will calculate three statuses <8 = invalid, 8-11 incl = valid 12+ = optimal */
   updateDOM(pwdValidity, pwdLength);
 };
@@ -16,19 +19,22 @@ const updateDOM = (pwdValidity, pwdLength) => {
   const progressBar = document.getElementById("pwdlength");
   /* const gameResult = document.createElement("gameResult"); */
   const progressFeedbackText = document.createElement("progressFeedbackText");
+  progressFeedbackText.setAttribute("id", "status");
   progressBar.value = pwdLength * 5;
+  var pwdValidity = "";
 
   if (pwdValidity === "invalid") {
-    progressFeedbackText.setAttribute("Should be longer");
+    pwdValidity = "Should be longer";
   } else if (pwdValidity === "valid") {
-    progressFeedbackText.setAttribute("Pretty good");
+    pwdValidity = "Pretty good";
   } else {
-    progressFeedbackText.setAttribute("Great");
+    pwdValidity = "Great";
   }
 
-  progressFeedbackText.setAttribute("id", "outcome");
   /*gameResult.textContent = outcome;
   document.body.appendChild(gameResult); */
+  progressFeedbackText.textContent = pwdValidity;
+  document.body.appendChild(progressFeedbackText);
 };
 
 const pwdInput = document.getElementById("pwd");
